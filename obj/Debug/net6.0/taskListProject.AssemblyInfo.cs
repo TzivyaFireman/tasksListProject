ada6655435497dacc0640e0ef8fb451dee86f910
Tzivya <tz0534199716@gmail.com>
2024-05-23 22:24:05 +0300

    add gitignore file, and remove unrelevant files

--- obj/Debug/net6.0/taskListProject.AssemblyInfo.cs
+++ obj/Debug/net6.0/taskListProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("taskListProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f7ce78e0f442cb11f1ee8fb3f8b48446725104f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca970cff3aaefd86867bd427d92dd8422cb4d7e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("taskListProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("taskListProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
